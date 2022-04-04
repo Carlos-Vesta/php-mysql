e3ae0e26246ede4d63d05418edec98ea99a5f116
Carlos Vesta <vesta.carlos@gmail.com>
2022-04-04 12:57:37 +0200

    functions/02-functions.php

---
+++ functions/02-functions.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Functions Statement</title>
+</head>
+
+<body>
+    <h4>Functions Statement</h4>
+    <br><br><br>
+
+    <?php
+        echo "<strong> FUNCTIONS </strong>" . "<br><br>";
+       
+        // Minha primeira funcao
+
+        function myFunction() {
+            $nome = "Carlos Vesta";
+            echo $nome;
+        }
+
+        myFunction();
+    ?>
+
+</body>
+
+</html>
