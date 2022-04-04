5ead38aa502e53735e9e3472999d79198a1f22ec
Carlos Vesta <vesta.carlos@gmail.com>
2022-04-04 12:45:25 +0200

    functions/01.introduction.php

---
+++ functions/01-introduction.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Functions Statement</title>
+</head>
+
+<body>
+    <h4>Functions Statement</h4>
+    <br><br><br>
+
+    <?php
+        echo "<strong> FUNCTIONS </strong>" . "<br><br>";
+        echo " - Função, é um bloco de código escrito em um programa para realizar alguma tarefa específica.";
+        echo "<br><br>";
+
+        echo "Ha dois (2) tipos de funcoes em PHP: <br>";
+        echo "<br><br>";
+        
+        echo "<strong> 1 - Funções embutidas do PHP (PHP Built-in Functions)</strong> <br>
+        Ex: <em>echo, print, var_dump, etc.</em>";
+        echo "<br><br>";
+
+        echo "<strong> 2 - Funções definidas pelo usuário PHP (PHP User Defined Functions)</strong> <br><br>
+        Ex: <strong>Camel case</strong> - <em>myFunction( )</em> <br>
+        <strong>Lower case</strong> - <em>my_function( )</em> <br>
+        <strong>Pascal case</strong> - <em>MyFunction( )</em> <br>";
+        echo "<br><br>";
+
+        echo "<strong> Porquê usar uma função</strong> <br>
+        - Reutilização da função <br>
+        - Fácil para detectar erros <br>
+        - Fácil manutenção";
+        echo "<br><br>";
+    ?>
+
+</body>
+
+</html>
